dfff63028bebdd57d9db51c7bdc868996523f0fc
Saja Malkawi <sajamalkawi95@gmail.com>
2021-09-10 19:47:41 +0300

    solve voting

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -27,14 +27,15 @@ export class HornedBeast extends Component {
 
             <Col>
                 <Card style={{ width: '18rem', margin: "20px" }} >
-                    <Card.Img alt='HornedBeast' class="card-img-top" title={this.props.Name} src={this.props.src} onClick={this.show} />
+                    <Card.Img alt='HornedBeast' class="card-img-top" title={this.props.Name} src={this.props.src} 
+                    onClick={this.imageClick} />
                     <Card.Body>
                         <Card.Title>{this.props.Name}</Card.Title>
                         
                            
                     
                         <Card.Text> 
-                            Likes { this.Like1}
+                            Likes {this.state.Like1}
                             <br/>
                             {this.props.disc}
                         </Card.Text>
